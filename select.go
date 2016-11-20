06de64fad1ca45e1217bc0cb9c58ef96d824e86e
MrLokans <trikster1911@gmail.com>
2016-11-20 08:06:57 +0300

    Added example of 'select' method.

---
+++ select.go
@@ -0,0 +1,31 @@
+package main
+
+// This short program demonstrates the usage of 'select'
+// to wait for multiple channels
+
+import "fmt"
+
+func fibonacci(c, quit chan int) {
+	x, y := 1, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("Выход")
+			return
+		}
+	}
+}
+
+func main() {
+	c := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 50; i++ {
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}()
+	fibonacci(c, quit)
+}
